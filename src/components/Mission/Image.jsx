815ecc17d35013b60065c02fe8c607d917c7e88e
maru0401 <maru0401.1679@gmail.com>
2023-08-06 23:51:36 +0900

    fix:画面サイズに応じて画像サイズが変わるように変更

--- src/components/Mission/Image.jsx
+++ src/components/Mission/Image.jsx
@@ -1,23 +1,18 @@
 import Webp from '../../assets/imgs/Mission/test5.jpg';
 import { CardMedia } from '@mui/material';
 
-const isSafariBasedBrowser = () => {
-  return /^((?!chrome|android).)*safari/i.test(navigator.userAgent);
-};
-
 const Image = () => {
-  const shouldApplyBackgroundAttachment = isSafariBasedBrowser();
-
   return (
     <CardMedia
       sx={{
         position: 'relative',
-        height: '900px',
+        height: '900px', // デフォルトの高さ
         backgroundImage: `url(${Webp})`,
-        ...(!shouldApplyBackgroundAttachment && {
-          backgroundAttachment: 'fixed',
-        }),
         filter: 'blur(8px)',
+        
+        '@media (max-width: 600px)': {
+          height: '600px', // 画面幅が 600px 以下の場合の高さ
+        },
       }}
       src={Webp}
     />
