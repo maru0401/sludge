dac1a7daedbeacd68f90c5d1fac212f0215f5e00
maruyama <131378253+maru0401@users.noreply.github.com>
2023-08-07 11:50:40 +0900

    Update News.js

--- src/components/News.js
+++ src/components/News.js
@@ -1,10 +1,6 @@
 import Content from './News/Content';
 import BackColor from './News/BackColor';
-import Picture1 from '../assets/imgs/News/test1.jpg';
-import Picture2 from '../assets/imgs/News/test2.jpg';
-import Picture3 from '../assets/imgs/News/test3.jpg';
-import Picture4 from '../assets/imgs/News/test4.jpg';
-import Picture5 from '../assets/imgs/News/test5.jpg';
+import Picture1 from '../assets/imgs/News/null.JPG';
 import { Box } from '@mui/material';
 import Slider from 'react-slick';
 import 'slick-carousel/slick/slick.css';
@@ -55,11 +51,11 @@ function News() {
         <Text />
         <Box sx={{ margin: '30px' }}>
           <Slider {...settings}>
-            <Content info={{ webp: Picture1, title: '江ノ島行ってきました' }} />
-            <Content info={{ webp: Picture2, title: 'Twitterはじめました' }} />
-            <Content info={{ webp: Picture3, title: 'Instagramはじめました' }} />
-            <Content info={{ webp: Picture4, title: 'サイトを公開しました' }} />
-            <Content info={{ webp: Picture5, title: 'Hello' }} />
+            <Content info={{ webp: Picture1, title: '準備中...' }} />
+            <Content info={{ webp: Picture1, title: '準備中...' }} />
+            <Content info={{ webp: Picture1, title: '準備中...' }} />
+            <Content info={{ webp: Picture1, title: '準備中...' }} />
+            <Content info={{ webp: Picture1, title: '準備中...' }} />
           </Slider>
         </Box>
         <MoreText />
