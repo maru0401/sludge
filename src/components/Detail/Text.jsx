dcc635717b0538dab741f0a8f2fd626a99cca562
maru0401 <maru0401.1679@gmail.com>
2023-08-20 20:14:23 +0900

    fix:画像が表示されない問題を修正

--- src/components/Detail/Text.jsx
+++ src/components/Detail/Text.jsx
@@ -5,9 +5,9 @@ import rehypeRaw from "rehype-raw";
 const Text = ({ text }) => {
   return (
     <Box sx={{
-      margin:'20px'
+      margin: '20px'
     }}>
-      <ReactMarkdown children={text} rehypePlugins={[rehypeRaw]} linkTarget="_blank" />
+      <ReactMarkdown children={text.replace('](/uploads/', '](https://admin.activated-sludge.xyz/uploads/')} rehypePlugins={[rehypeRaw]} linkTarget="_blank" />
     </Box>
   );
 }
